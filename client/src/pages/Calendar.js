b01be1302fe0db487dee7e8817af3dd004374096
juanstro <juanfcastro03@gmail.com>
2023-03-13 19:10:52 -0700

    Implemented start/end time constraint buttons, which modifies displayed and saved events. Implemented revert button, which unsaves all saved events and reverts calendar to original events.
    Need to fix drag, drop, and resize

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -22,9 +22,11 @@ export default function Calendar() {
     const [confirmedAvailability, setConfirmedAvailability] = useState({});
     const [meetingID, setMeetingID] = useState(null);
     const [meetingMemberIDs, setMeetingMemberIDs] = useState(null);
+
+    const [ogAvail, setOgAvail] = useState([]); // can maybe change back to an array & push events
     
-    const [minTime, setMinTime] = useState('06:00:00');
-    const [endTime, setEndTime] = useState('22:00:00');
+    const [minTime, setMinTime] = useState('00:00');
+    const [endTime, setEndTime] = useState('23:59');
 
     // When user loads home screen, initialize Google Calendar API
     useEffect(() => {
@@ -109,12 +111,15 @@ export default function Calendar() {
     // Transform availability from Google Calendar API into displayed availabilities on calendar GUI
     const loadValues = async (tempAvailability) => {
         await new Promise(r => setTimeout(r, 100));
+        console.log("load values called");
         if (tempAvailability) {
             let _events = [];
+            let _ogevents = [];
             const timeNow = Date.now();
             for (const start_end of tempAvailability) {
                 // testing: will show all availability before too
-                if (start_end[1] < timeNow) continue;
+                // if (start_end[1] < timeNow) continue; // this gets every availability before current day
+                if (start_end[0] < timeNow) continue; // this is not accurate, does not get some availability
 
                 // if event spans multiple days
                 if (!isOneDay(start_end)) {
@@ -136,21 +141,23 @@ export default function Calendar() {
                                 end: e,
                                 id: uuidv4(),
                                 saved: false,
+                                backgroundColor: "green"
                             };
                             _events.push(_event);
-                            setDisplayedAvailability(_events);
+                            _ogevents.push({..._event});
                             s = new Date(s.getFullYear(), s.getMonth(), s.getDate() + 1, 0, 0, 0)
                         }
-                        else {
+                        else { // e >= end
                             const _event = {
                                 title: "Available",
                                 start: s,
-                                end: start_end[1],
+                                end: start_end[1], 
                                 id: uuidv4(),
                                 saved: false,
+                                backgroundColor: "green"
                             };
                             _events.push(_event);
-                            setDisplayedAvailability(_events);
+                            _ogevents.push({..._event});
                             break;
                         }
                     }
@@ -163,11 +170,14 @@ export default function Calendar() {
                         end: start_end[1],
                         id: uuidv4(),
                         saved: false,
+                        backgroundColor: "green"
                     };
                     _events.push(_event);
-                    setDisplayedAvailability(_events);
+                    _ogevents.push({..._event});
                 };
             }
+            setDisplayedAvailability(_events);
+            setOgAvail(_ogevents);
         }
     }
 
@@ -187,30 +197,44 @@ export default function Calendar() {
     // Save displayed events to savedAvailability hook when user clicks on event from calendar GUI
     const handleEventClick = (arg) => {
         const saved_events = {...savedAvailability };
+
+        const temp_displayed = [...displayedAvailability];
+        const idx = temp_displayed.findIndex(event => event.id === arg.event.id);
+
         if (arg.event.extendedProps.saved) {
             arg.event.setExtendedProp("saved", false);
             arg.event.setProp("backgroundColor", "green");
             delete saved_events[arg.event.id];
+            temp_displayed[idx].saved = false;
+            temp_displayed[idx].backgroundColor = "green";
         }
         else {
             arg.event.setExtendedProp("saved", true);
             arg.event.setProp("backgroundColor", "red");
             saved_events[arg.event.id] = [arg.event.start, arg.event.end];
+            temp_displayed[idx].saved = true;
+            temp_displayed[idx].backgroundColor = "red";
         }
         setSavedAvailability(saved_events);
+        setDisplayedAvailability(temp_displayed);
     }
 
     const handleEventDrop = (arg) => {
         const saved_events = {...savedAvailability };
 
-        // if the event was saved pre-drop
+        const temp_displayed = [...displayedAvailability];
+        const idx = temp_displayed.findIndex(event => event.id === arg.event.id);
+
         if (arg.oldEvent.extendedProps.saved) {
             if (arg.oldEvent.id === arg.event.id) console.log("same id! was expected");
             // replace the old event with the new, by key = id
             delete saved_events[arg.oldEvent.id]
             saved_events[arg.event.id] = [arg.event.start, arg.event.end]
+            temp_displayed[idx].start = arg.event.start;
+            temp_displayed[idx].end = arg.event.end;
         }
         setSavedAvailability(saved_events);
+        setDisplayedAvailability(temp_displayed);
     }
 
     const handleEventResize = (arg) => {
@@ -236,10 +260,99 @@ export default function Calendar() {
 
     const handleStartChange = (event) => {
         setMinTime(event.target.value);
+
+        let displayed_temp = [];
+        let saved_temp = {...savedAvailability};
+
+        const [start_hr, start_min] = (event.target.value).split(":");
+
+        for (const e of displayedAvailability) {
+            const new_start = new Date(e.start);
+            new_start.setHours(start_hr);
+            new_start.setMinutes(start_min);
+            if (e.start < new_start) { // e.start is before start constraint
+                if (e.end <= new_start) { //e.end is before or at start constraint; don't display
+                    delete saved_temp[e.id];
+                    continue;
+                } 
+                const new_e = {
+                    title: "Available",
+                    start: new_start,
+                    end: e.end, 
+                    id: e.id,
+                    saved: e.saved,
+                    backgroundColor: e.backgroundColor
+                };
+                console.log("Saved?", e.saved);
+                if (e.saved) {
+                    console.log("event previously saved")
+                    delete saved_temp[e.id];
+                    saved_temp[e.id] = [new_start, e.end];
+                };
+                displayed_temp.push(new_e);
+            }
+            else { // e.start is after or at the start constraint; no changes needed
+                displayed_temp.push(e);
+            }
+        }
+        setDisplayedAvailability(displayed_temp);
+        setSavedAvailability(saved_temp);
     }
 
     const handleEndChange = (event) => {
         setEndTime(event.target.value);
+
+        let displayed_temp = [];
+        let saved_temp = {...savedAvailability};
+        
+        const [end_hr, end_min] = (event.target.value).split(":");
+
+        for (const e of displayedAvailability) {
+            const new_end = new Date(e.end);
+            new_end.setHours(end_hr);
+            new_end.setMinutes(end_min);
+
+            if (e.end > new_end) { // e.end after end constraint
+                if (e.start >= new_end) { // e.start after or at end constraint; don't display
+                    delete saved_temp[e.id];
+                    continue;
+                }
+                const new_e = {
+                    title: "Available",
+                    start: e.start,
+                    end: new_end, 
+                    id: e.id,
+                    saved: e.saved,
+                    backgroundColor: e.backgroundColor
+                };
+                if (e.saved) {
+                    delete saved_temp[e.id];
+                    saved_temp[e.id] = [new_e.start, new_e.end];
+                }
+                displayed_temp.push(new_e);
+            }
+            else { // e.end before or at the end constraint; no changes needed
+                displayed_temp.push(e);
+            }
+        }
+        setDisplayedAvailability(displayed_temp);
+        setSavedAvailability(saved_temp);
+    }
+
+    const revertChanges = () => {
+        console.log("reverting changes");
+        let temp_avail = [];
+
+        //setDisplayedAvailability([...ogAvail]);
+        for (let e of ogAvail) {
+            temp_avail.push({...e});
+        }
+
+        setDisplayedAvailability(temp_avail);
+        setSavedAvailability([]);
+
+        setMinTime('00:00');
+        setEndTime('23:59');
     }
 
     const checkConfirmedAvailability = () => {
@@ -247,6 +360,7 @@ export default function Calendar() {
         for (let event_id in savedAvailability) {
             console.log("event data: ", savedAvailability[event_id]);
         }
+        console.log("saved events total:", Object.keys(savedAvailability).length);
     }
 
     return (
@@ -260,24 +374,29 @@ export default function Calendar() {
             <button onClick={confirmAvailability}>{ eventsData ? "Confirm availability" : ""}</button>
 
             <button onClick={checkConfirmedAvailability}>check confirmed availability</button>
+            
+            {eventsData && (
+                <div>
+                    <br/>
+                    <label htmlFor="start-time-input"></label>
+                    <input
+                    id="start-time-input"
+                    type="time"
+                    value={minTime}
+                    onChange={handleStartChange}
+                    />
 
-            <div>
-                <label htmlFor="start-time-input"></label>
-                <input
-                id="start-time-input"
-                type="time"
-                value={minTime}
-                onChange={handleStartChange}
-                />
+                    <label htmlFor="end-time-input"></label>
+                    <input
+                    id="end-time-input"
+                    type="time"
+                    value={endTime}
+                    onChange={handleEndChange}
+                    />
 
-                <label htmlFor="end-time-input"></label>
-                <input
-                id="end-time-input"
-                type="time"
-                value={endTime}
-                onChange={handleEndChange}
-                />
-            </div>
+                    <button onClick={revertChanges}> Revert Changes </button>
+                </div>
+            )}
 
             <calendar>
                 <div class="square"></div>
@@ -302,7 +421,8 @@ export default function Calendar() {
                 eventResizableFromStart={true}
                 eventDrop={handleEventDrop}
                 eventResize={handleEventResize}
-                
+                eventOverlap={false}
+                //eventAfterRender={handleEventAfterRender}
                 />
             </calendar>
         
